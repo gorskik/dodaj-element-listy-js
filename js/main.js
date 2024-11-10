9896634a6b54eab6c9f4329e7b16c23726d2eb38
gorskik <kamilgr80@gmail.com>
2024-11-10 18:53:33 +0100

    dodaj-element-listy-js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function addItem() {
+            const list = document.getElementById('items');
+            const currentItemsCount = list.getElementsByTagName('li').length;
+
+            const newItem = document.createElement('li');
+            newItem.classList.add('item');
+            newItem.textContent = `Item ${currentItemsCount + 1}`;
+
+            list.appendChild(newItem);
+        }
+
+        document.getElementById('addButton').addEventListener('click', addItem);
+
